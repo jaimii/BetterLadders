75e357e8155cfc502b59adc8e4293f36a3fdad12
JustJaimii <jancarlodalacat@gmail.com>
2026-04-22 14:13:06 +0800

    Intial Commit.

---
+++ src/main/java/project/kompass/betterLadders/BetterLadders.java
@@ -0,0 +1,17 @@
+package project.kompass.betterLadders;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterLadders extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
