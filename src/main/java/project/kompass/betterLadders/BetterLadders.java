0f5da592f043e94947b33cf9d0b52004ed016282
JustJaimii <jancarlodalacat@gmail.com>
2026-04-24 00:22:28 +0800

    Improve climbing logic.

--- src/main/java/project/kompass/betterLadders/BetterLadders.java
+++ src/main/java/project/kompass/betterLadders/BetterLadders.java
@@ -2,12 +2,13 @@ package project.kompass.betterLadders;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BetterLadders extends JavaPlugin {
+public class BetterLadders extends JavaPlugin {
 
+    @Override
     public void onEnable() {
-        // Registering the listener (this class) so the events work
         getServer().getPluginManager().registerEvents(new LadderListener(), this);
-        getLogger().info("Get ready to climb!");
+
+        getLogger().info("Climbing better!");
     }
 
     @Override
